37ddbaa096d82c24dc6201e9d2a5bb701f94708a
shrugs <mattgcondon@gmail.com>
2023-05-09 09:25:16 -0400

    feat: local batch rendering

--- index.js
+++ index.js
@@ -1,64 +1,103 @@
-const express = require("express");
-const puppeteer = require("puppeteer")
-const sharp = require('sharp');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.status(200).end();
-});
-
-const ARWEAVE_URI = /^https:\/\/arweave.net\/4Tz9sZQ3ilCbO5dCdmVkeE-SSSF4nXNPu7Gp9lmTqjM\/(\d).html\?.+$/
+const { ethers } = require("ethers");
+const { Cluster } = require('puppeteer-cluster');
+const _ = require('lodash')
+const {existsSync} = require('fs')
 
 const VIEWPORT_FOR_TEMPLATE = {
-  0: { width: 1000, height: 1200 }, // Fidenza
-  1: { width: 1600, height: 900 }, // Himminn
-  2: { width: 1024, height: 1024 }, // Qwerty
-  3: { width: 1024, height: 1024 }, // Ringers
-  4: { width: 1024, height: 1024 }, // LGG
-  5: { width: 1024, height: 1024 }, // Squiggle
-  6: { width: 1024, height: 1024 }, // Window
+  0: { width: 1000, height: 1200 },  // Fidenza
+  1: { width: 1024, height: 1024 },  // Cargo
+  2: { width: 1024, height: 1024 },  // Qwerty
+  3: { width: 1024, height: 1024 },  // Ringers
+  4: { width: 1024, height: 1024 },  // LGG
+  5: { width: 1024, height: 1024 },  // Squiggle
+  6: { width: 1024, height: 1024 },  // Window
+  7: { width: 1024, height: 1024 },  // fake it till you make it
+  8: { width: 1024, height: 1024 },  // contractions
+  9: { width: 1024, height: 1024 },  // intersections
+  10: { width: 1024, height: 1024 }, // tide predictor
 }
 
-app.get(["/:uri"], async (req, res) => {
-  const uri =  Buffer.from(req.params["uri"], 'base64').toString('ascii');
+const CONCURRENCY = {
+  0: 10,  // Fidenza
+  1: 4,  // Cargo
+  2: 10,  // Qwerty
+  3: 10,  // Ringers
+  4: 1,  // LGG
+  5: 10,  // Squiggle
+  6: 10,  // Window
+  7: 10,  // fake it till you make it
+  8: 10,  // contractions
+  9: 10,  // intersections
+  10: 10, // tide predictor
+}
 
-  const matches = uri.match(ARWEAVE_URI);
-  if (!matches) return res.status(400).end();
+// https://arweave.net/SGxR0nspX2CdQsxYtMyval7VS15Flc6VHBq9dNdgIVE/0.html
+const makeTemplateUri = (templateId, tokenId, hash) => `http://localhost:8000/${templateId}.html?${new URLSearchParams({
+  payload: Buffer.from(JSON.stringify({ tokenId, hash })).toString('base64')
+})}`
 
-  const template = parseInt(matches[1]);
-  const viewport = VIEWPORT_FOR_TEMPLATE[template];
-  if (!viewport) return res.status(400).end();
+const pathForTokenAndTemplate = (tokenId, templateId) => `./output/${tokenId}-${templateId}.png`
+const hashForTokenId = (tokenId) => ethers.utils.keccak256(ethers.utils.toUtf8Bytes(`RANDOM${tokenId}`))
 
-  // https://github.com/fly-apps/puppeteer-js-renderer/blob/master/index.js
-  const browser = await puppeteer.launch({
-    executablePath: process.env.NODE_ENV === 'production'
-      ? '/usr/bin/google-chrome'
-      : undefined,
-    headless: "new",
-    args: [
-      '--disable-gpu',
-      '--use-gl=egl',
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage'
-    ],
+async function main() {
+  const templateId = parseInt(process.argv[2]);
+  console.log(`rendering template ${templateId}`)
+  const tokens = _.times(1000).map(tokenId => ({
+    tokenId,
+    hash: hashForTokenId(tokenId),
+  }))
+
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: CONCURRENCY[templateId],
+    puppeteerOptions: {
+      headless: 'new',
+      args: [
+        '--use-gl=egl',
+        '--use-cmd-decoder=passthrough',
+        '--no-sandbox'
+      ],
+    }
   });
-  const page = await browser.newPage();
 
-  await page.setViewport(viewport);
+  cluster.on('taskerror', (err, data, willRetry) => {
+    if (willRetry) {
+      console.warn(`Encountered an error while crawling ${JSON.stringify(data, null)}. ${err.message}\nThis job will be retried`);
+    } else {
+      console.error(`Failed to crawl ${JSON.stringify(data, null)}: ${err.message}`);
+    }
+  });
 
-  await page.goto(uri, { waitUntil: template === 5 ? 'domcontentloaded' : 'networkidle0' });
+  await cluster.task(async ({ page, data: { templateId, tokenId, hash } }) => {
+    const uri = makeTemplateUri(templateId, tokenId, hash);
+    console.log(`token ${tokenId}, template ${templateId} at ${uri}`);
+    const viewport = VIEWPORT_FOR_TEMPLATE[templateId];
 
-  const image = await page.screenshot({type: 'png'})
+    await page.setViewport(viewport);
+    await page.goto(uri, {
+      waitUntil: templateId === 4 ? 'domcontentloaded' : undefined,
+      timeout: templateId === 4 ? 10_000 : 5_000,
+    });
 
-  await browser.close();
+    await page.screenshot({
+      type: 'png',
+      path: pathForTokenAndTemplate(tokenId, templateId),
+    })
+  });
 
-  res.setHeader('Content-Type', 'image/png');
-  res.setHeader("Content-Disposition", `inline; filename="wrapped-tokenhash.png"`);
-  res.setHeader("Cache-Control", `public, s-maxage=${6 * 30 * 24 * 60 * 60}`);
+  for (const {tokenId, hash} of tokens) {
+    if (!existsSync(pathForTokenAndTemplate(tokenId, templateId))) {
+      cluster.queue({ templateId, tokenId, hash });
+    }
+  }
 
-  res.status(200).send(image)
-});
+  await cluster.idle();
+  await cluster.close();
+}
 
-app.listen(port, () => console.log(`listening on port ${port}!`));
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
